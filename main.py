6686ecf7801bcb71791e1e0530d9a42d873ae85c
Elijah <56296903+sinaca@users.noreply.github.com>
2024-01-05 23:41:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-from kivy.app import App
-from kivy.uix.button import Button
+from kivy.app import App 
+from kivy.uix.boxlayout import Boxlayout 
+from kivy.uix.label import Label 
+from player import vibrator 
+from kivy.core.video import Video 
+from kivy.uix.widget import widget
+from kivy.uix.button import Button 
+class start(Widget): 
+    def __init__(self):
+        super(start, self). __init__(**kwgs)
+           
+
+ 
+        
 class TestApp(App):
     def build(self):
         return Button(text='Hello world')

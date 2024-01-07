944b03c75c028cffd228ac9bc09ebe98d1974c45
Elijah <56296903+sinaca@users.noreply.github.com>
2024-01-07 11:00:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from kivy.uix.video import Video
 from kivy.uix.boxlayout import BoxLayout 
 from kivy.uix.label import Label 
 from plyer import vibration
-from kivy.core.audio import Audio 
+from kivy.core.audio import SoundLoader
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button 
 class Start(Widget):
